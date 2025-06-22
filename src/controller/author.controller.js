52785ef70bedf9cb9c3aadcc9325518bd4937503
David1991Grig <davoaleks1991@gmail.com>
2025-06-22 18:41:21 +0300

    with transaction

--- src/controller/author.controller.js
+++ src/controller/author.controller.js
@@ -1,4 +1,5 @@
 import {Author, Book} from "../model/index.js";
+import {sequelize} from "../config/database.js";
 
 export const findBookAuthors = async (req, res) => {
     try {
@@ -21,18 +22,21 @@ export const findBookAuthors = async (req, res) => {
     }
 }
 export const removeByAuthors = async (req, res) => {
+    const t = await sequelize.transaction();
     try {
         const {author} = req.params;
-        const remAuthor = await  Author.destroy({
-            where: { name: author }
+        const remAuthor = await Author.destroy({
+            where: { name: author },
+            transaction: t
         });
         if (!remAuthor) {
+            await t.rollback();
             return res.status(404).json({error: "Author not found"});
         }
-
+        await t.commit()
         return res.json(remAuthor);
-
     }catch (e){
+        await t.rollback();
         console.error('Error finding book by author', e);
         return res.status(500).json({
             error: 'Failed to find book by author'
